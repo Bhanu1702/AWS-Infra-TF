e8dfe4b30ffe52c3863697990239883b184a313a
Bhanu1702 <bhanu170221@gmail.com>
2023-03-15 16:29:55 +0530

    First Commit

---
+++ igw.tf
@@ -0,0 +1,8 @@
+# creating Internet Gateway
+
+resource "aws_internet_gateway" "demogateway" {
+  vpc_id = "${aws_vpc.demovpc.id}"
+   tags ={
+     Name ="IGW"
+}
+}
---
+++ main.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAVVTXODIPPGWNVZIL"
+  secret_key = "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"
+}
